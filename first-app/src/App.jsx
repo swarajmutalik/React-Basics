eb1881efb644a442d4715c31339692c8f8affc9b
swarajmutalik <swarajmutalik@gmail.com>
2025-03-15 13:02:52 +0530

    first-app

---
+++ first-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Card from "./components/card"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+      <div className="cards">
+        <Card title="card 1" description="card-1 description"/>
+        <Card title="card 2" description="card-2 description"/>
+        <Card title="card 3" description="card-3 description"/>
+        <Card title="card 4" description="card-4 description"/>
+      </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
